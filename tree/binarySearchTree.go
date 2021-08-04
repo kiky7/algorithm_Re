7bcbdec026f833911e52d2b198814c6c7296394a
wumuchun <403660598@qq.com>
2021-08-04 19:50:46 +0800

    二叉搜索树中第K小的元素

---
+++ tree/binarySearchTree.go
@@ -0,0 +1,17 @@
+/*
+ * @author:kiky
+ * @date: 2021/8/4 7:48 下午
+**/
+
+package tree
+
+/**
+ * @Description:  二叉搜索树中第K小的元素--230
+ * @param root
+ * @param k
+ * @return int
+ */
+func kthSmallest(root *TreeNode, k int) int {
+
+	return 1
+}
